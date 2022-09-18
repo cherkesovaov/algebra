54752b60b217ad7b08b0effff6bb26ef953db09a
cherkesovaov <101700852+cherkesovaov@users.noreply.github.com>
2022-09-18 15:41:30 +0300

    C=BA

--- main.c
+++ main.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() //B=A^(-1)
+int main()//C=BA
 {
     int n;
     scanf("%d",&n);
-    int A[n],B[n];
+    int A[n],B[n],C[n];
     int i;
     for(i=0;i<n;i++){
         scanf("%d",&A[i]);
     }
     for(i=0;i<n;i++){
-        B[A[i]-1]=i+1;
+        scanf("%d",&B[i]);
     }
     for(i=0;i<n;i++){
-        printf("%d ",B[i]);
+        C[i]=B[A[i]-1];
+    }
+    for(i=0;i<n;i++){
+        printf("%d ",C[i]);
     }
     return 0;
 }
