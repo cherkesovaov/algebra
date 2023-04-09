a156a432426ef4ae323b3dd68539dc9d79557528
cherkesovaov <101700852+cherkesovaov@users.noreply.github.com>
2023-04-09 18:57:45 +0300

    rucksack 4.0
    Try to read from file

--- rucksack.m
+++ rucksack.m
@@ -1,14 +1,18 @@
 function rucksack
   clear;
   clc;
-  n=10;
+  file='C:\Users\Olga\Desktop\new_object\data\ks_4_0.txt';
+  [n, k]=dlmread(file,' ',0,0)
+  weight=dlmread(file, '\n', 1,0)
+  cost=dlmread(file, '\n', 1,1)
+ % n=4;
   m=5;
-  k=4;
+  %k=4;
   cmax=0;
   rfinal=zeros(1,10);
-  weight=[1:10];
-  cost=[2:2:20];
-  W=40;
+  %weight=[4, 5, 8, 3];
+  %cost=[8, 10, 15, 4];
+  W=11;
   C=sum(cost);
   for i=1:m
     A(i,:)=randi(2,1,n)-ones(1,n);
@@ -23,7 +27,6 @@ function rucksack
   it=1;
   while((cmax<C*0.8)&(it<6))
     if(it==0)
-      %'plused'
       it=1;
     endif
     l=1;
@@ -45,12 +48,9 @@ function rucksack
       endfor
     endfor
     A=B;
-    B;
     if(it>0)
-      %'plused'
       it=it+1;
     endif
-    it;
   endwhile
   cmax
   rfinal
