f0a479838da3068b1258cc4a791bd266f06a4c16
cherkesovaov <101700852+cherkesovaov@users.noreply.github.com>
2023-04-16 13:05:43 +0300

    Greedy algorithm.

--- greedy_rucksack.m
+++ greedy_rucksack.m
@@ -1,13 +1,23 @@
 function greedy_rucksack
   clear;
   clc;
-  n=6;
-  weight=[5, 15, 78, 56, 1, 9];
-  cost=[67, 6767, 5786, 445, 999, 2122];
+  file="c:\\users\\olga\\desktop\\new_object\\data\\ks_10000_0";
+  l=zeros(1,2);
+  l=dlmread(file, " ",0,0);
+  n=l(1,1);
+  W=l(1,2);
+  weight=zeros(1,n);
+  cost=zeros(1,n);
+  for(i=1:n)
+    l=dlmread(file," ",i,0);
+    weight(i)=l(1,2);
+    cost(i)=l(1,1);
+  endfor
+  C=sum(cost);
+  cmax=0;
   rkg=cost./weight;
   put=zeros(1,n);
   %sum(weight)
-  V=100;
   f=1;
   S=0;
   while(f)
@@ -15,14 +25,14 @@ function greedy_rucksack
     M=0;
     p=0;
     for(i=1:n)
-      if((rkg(i)>M)&&(put(i)==0)&&(weight(i)<V))
+      if((rkg(i)>M)&&(put(i)==0)&&(weight(i)<W))
         M=rkg(i);
         p=i;
       endif
     endfor
     if(p>0)
       put(p)=1;
-      V=V-weight(p);
+      W=W-weight(p);
       S=S+cost(p);
       f=1;
       M=0;
@@ -30,6 +40,7 @@ function greedy_rucksack
     endif
   endwhile
   S
+  W
   %sum(cost)
   put
 endfunction
