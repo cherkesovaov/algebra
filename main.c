d53b84fdc9a0b96cf66cccbba5add7d70a33abb0
cherkesovaov <101700852+cherkesovaov@users.noreply.github.com>
2023-06-20 15:19:05 +0300

    TSP
    Commented code making output file. Another commented code passing all tests, but it doesn't works.

--- main.c
+++ main.c
@@ -1,23 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
-int main()//C=BA
+int main()
 {
-    int n;
-    scanf("%d",&n);
-    int A[n],B[n],C[n];
-    int i;
-    for(i=0;i<n;i++){
-        scanf("%d",&A[i]);
+    FILE *file, *data, *ans, *output, *table,*names;
+    //names=fopen("C:/Users/Olga/Desktop/progs/data/data/names","r");
+    //int iter;
+    //for(iter=0;iter<76;iter++){
+    //char name[15];
+    //fscanf(names,"%s", name);
+    //char path[]="C:/Users/Olga/Desktop/progs/data/data/";
+    //strcat(path,name);
+    //strcat(path,".txt");
+    char* path="C:/Users/Olga/Desktop/progs/data/data/tsp_70_1";
+    //char* name="tsp_51_1";
+    file=fopen(path,"r");
+    int i,j,v;
+    fscanf(file,"%d",&v);
+    double x[v],y[v],d[v][v];
+    for(i=0;i<v;i++){
+        fscanf(file,"%lf",&x[i]);
+        fscanf(file,"%lf", &y[i]);
     }
-    for(i=0;i<n;i++){
-        scanf("%d",&B[i]);
+    for(i=0;i<v;i++){
+        for(j=0;j<v;j++){
+            d[i][j]=sqrt(pow((x[i]-x[j]),2)+pow((y[i]-y[j]),2));
+        }
     }
-    for(i=0;i<n;i++){
-        C[i]=B[A[i]-1];
+    data=fopen("C:/Users/Olga/Desktop/progs/data/data/data.dzn","w+");
+    fprintf(data,"v=");
+    fprintf(data,"%d",v);
+    fprintf(data,";");
+    fprintf(data,"\n");
+    fprintf(data,"d=[| ");
+    for(i=0;i<v;i++){
+        fprintf(data,"\n");
+        for(j=0;j<v-1;j++){
+           fprintf(data,"%lf",d[i][j]);
+           fprintf(data," , ");
+        }
+        fprintf(data,"%lf",d[i][v-1]);
+        fprintf(data,"|");
     }
-    for(i=0;i<n;i++){
-        printf("%d ",C[i]);
-    }
+    fprintf(data,"];");
+    fclose(data);
+    fclose(file);
+    //system("minizinc --solver Gecode C:/Users/Olga/Desktop/progs/TSP.mzn C:/Users/Olga/Desktop/progs/data/data/data.dzn >C:/Users/Olga/Desktop/progs/data/data/output.log");
+    //ans=fopen("C:/Users/Olga/Desktop/progs/data/data/ans","w+");
+    //output=fopen("C:/Users/Olga/Desktop/progs/data/data/output.log","r");
+    //printf("Done!");
+    //int r;
+    //double r1;
+    //fscanf(output,"[%d, ",&r);
+    //fprintf(ans,"%d ",r);
+    //for(i=2;i<v+1;i++){
+        //fscanf(output,"%d, ",&r);
+        //fprintf(ans,"%d ",r);
+    //}
+    //fscanf(output,"%d]\n",&r);
+    //fprintf(ans,"%d\n",r);
+    //fscanf(output,"%lf",&r1);
+    //fprintf(ans,"%lf",r1);
+    //fclose(output);
+    //fclose(ans);
+    //table=fopen("C:/Users/Olga/Desktop/progs/data/data/table","w+");
+    //fprintf(table,"%s",name);
+    //fprintf(table,"     %lf\n",r1);
+    //}
     return 0;
 }
